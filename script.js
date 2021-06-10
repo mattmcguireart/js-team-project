c569e853963c5c5619d01290a13da1974321c346
Teri Klosky <t.klosky@yahoo.com>
2021-06-10 09:05:30 -0400

    opening modal

--- script.js
+++ script.js
@@ -1,8 +1,46 @@
 "use strict";
 
+// shuffle cards
+// flip cards
+// match cards
+// update score
+// timer when start is clicked
+// losing modal
+// restart
+// congratulations modal
+
+let modal = document.querySelector(".modal");
 let cardsOuter = document.querySelectorAll(".card-outer");
-let cardsInner = document.querySelectorAll("card-inner");
+let cards = document.querySelectorAll(".card-inner");
+let images = [];
 
-let randonNumber = Math.floor(Math.random() * 16);
+let rNumber = 0;
 
-const shuffle = () => {};
+const toggleModal = () => {
+  modal.classList.toggle("hide");
+};
+
+modal.addEventListener("click", (e) => {
+  if (
+    e.target.classList.contains("modal") ||
+    e.target.classList.contains("start")
+  ) {
+    modal.classList.toggle("hide");
+  }
+});
+
+const randomNumber = () => {
+  rNumber = Math.floor(Math.random() * 16);
+  return rNumber;
+};
+
+const shuffle = () => {
+  let counter = 0;
+  let usedNumbers = [];
+  while (counter < 9);
+  let divA = randomNumber();
+  let divB = randomNumber();
+  image = images[randomNumber()];
+  cardsOuter[divA].setAttribute("src", image);
+  cardsOuter[divB].setAttribute("src", image);
+};
