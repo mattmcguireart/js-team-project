83cf8264b4b8f5106896910e580ca5f14c83238a
llynch10 <luke.s.lynch@gmail.com>
2021-06-10 11:51:39 -0400

    shuffle and flip

--- script.js
+++ script.js
@@ -1,6 +1,5 @@
 "use strict";
 
-// shuffle cards
 // flip cards
 // match cards
 // update score
@@ -10,9 +9,29 @@
 // congratulations modal
 
 let modal = document.querySelector(".modal");
+let cardContainer = document.querySelector(".card-container");
 let cardsOuter = document.querySelectorAll(".card-outer");
 let cards = document.querySelectorAll(".card-inner");
-let images = [];
+let backs = document.querySelectorAll(".back");
+
+let images = [
+  "6088f9a30da8f40018033e29.jpg",
+  "cant.jpeg",
+  "cat.jpg",
+  "keanu.jpg",
+  "leo.jpg",
+  "mj.jpg",
+  "pika.jpg",
+  "yellow.png",
+  "6088f9a30da8f40018033e29.jpg",
+  "cant.jpeg",
+  "cat.jpg",
+  "keanu.jpg",
+  "leo.jpg",
+  "mj.jpg",
+  "pika.jpg",
+  "yellow.png",
+];
 
 let rNumber = 0;
 
@@ -34,13 +53,26 @@ const randomNumber = () => {
   return rNumber;
 };
 
-const shuffle = () => {
-  let counter = 0;
-  let usedNumbers = [];
-  while (counter < 9);
-  let divA = randomNumber();
-  let divB = randomNumber();
-  image = images[randomNumber()];
-  cardsOuter[divA].setAttribute("src", image);
-  cardsOuter[divB].setAttribute("src", image);
+const shuffle = (array) => {
+  let oldElement;
+  for (let i = array.length - 1; i > 0; i--) {
+    let rand = Math.floor(Math.random() * 16);
+    oldElement = array[i];
+    array[i] = array[rand];
+    array[rand] = oldElement;
+  }
+  for (let i = 0; i < backs.length; i++) {
+    backs[i].setAttribute("src", images[i]);
+  }
 };
+
+shuffle(images);
+
+const assignImg = () => {};
+
+const cardFlip = (e) => {
+  console.log(e.target.parentNode);
+  e.target.parentNode.classList.toggle("flip-card");
+};
+
+cardContainer.addEventListener("click", cardFlip);
